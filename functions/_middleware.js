95613750dc6642fd77f5ec75aca236a3910ff7e9
L.Broussal <luciano.broussal@gmail.com>
2025-04-17 18:27:45 +0200

    Bloque /cart via middleware

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -2,9 +2,9 @@ export async function onRequest(context) {
   const url = new URL(context.request.url);
 
   if (url.pathname === "/cart") {
-    return new Response("Interdit d'accès direct sans être authentifié", {
+    return new Response("Interdit d'accès direct sans être authentifié. Interception edge function pas de worker", {
       status: 403,
-      headers: { "Content-Type": "text/plain" },
+      headers: { "Content-Type": "text/plain; charset=utf-8"},
     });
   }
 
