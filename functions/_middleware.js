ea8348d700b46cb4290bc1f275a23dec61be94f2
L.Broussal <luciano.broussal@gmail.com>
2025-04-17 18:25:24 +0200

    Bloque /cart via middleware

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,12 +1,12 @@
 export async function onRequest(context) {
   const url = new URL(context.request.url);
 
- // if (url.pathname === "/cart") {
-    return new Response("Interdit", {
+  if (url.pathname === "/cart") {
+    return new Response("Interdit d'accès direct sans être authentifié", {
       status: 403,
       headers: { "Content-Type": "text/plain" },
     });
- // }
+  }
 
   // Toutes les autres routes continuent normalement
   return context.next();
